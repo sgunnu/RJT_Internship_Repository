8d60a214ea81312c09c96ee0193499a803b51d6c
sgunnu <38260216+sgunnu@users.noreply.github.com>
2018-04-12 12:03:55 -0500

    Update and rename EmployeeServiceImpl.java to EmployeeServiceBasicPlan.java

---
+++ Day_2_Files/Day_2_Files/Day_2_Files/EmployeeServiceBasicPlan.java
@@ -0,0 +1,10 @@
+package com.assignment_2;
+
+public class EmployeeServiceBasicPlan implements EmployeeService 
+{
+    @Override
+    public String plan() 
+    {
+        return "Healthy plan";
+    }
+}
